df63d95f14312d9fbfd2b8317727257f7a8db942
Gabi <gabrielatryba00@gmail.com>
2022-05-24 20:47:09 +0200

    Dzialajaca wersja phoneCalls

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+import csv
+import unittest
+from unittest import TestCase
+
+class SprawdzDzwoniacegoTest(TestCase):
+      def test_czy_abonent_najczesciej_dzwoniacy_rozpoznany_poprawnie(self):
+        mp = MenadzerPolaczen("phoneCalls.csv")
+        wynik = mp.pobierz_najczesciej_dzwoniacego()
+        self.assertEqual((226,5), wynik)
+
+
+class MenadzerPolaczen:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data_dict = self.read_data()
+
+    def read_data(self):
+        calls_dict_sum = dict()
+        with open(self.filename, 'r') as fin:
+            reader = csv.reader(fin, delimiter= ",")
+            headers = next(reader)
+
+            for row in reader:
+                from_subsr = int(row[0])
+                if from_subsr not in calls_dict_sum:
+                    calls_dict_sum[from_subsr] = 0
+                calls_dict_sum[from_subsr] += 1
+        return calls_dict_sum
+
+    def pobierz_najczesciej_dzwoniacego(self):
+        return max(self.data_dict.items(), key= lambda x: x[1])
+
 
 if __name__ == "__main__":
-    pass
+    nazwa_pliku = input()
+    print(nazwa_pliku)
+    mp = MenadzerPolaczen(nazwa_pliku)
+    wynik = mp.pobierz_najczesciej_dzwoniacego()
+    print(wynik)
